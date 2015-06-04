d23b2bd734d0918bb7b2c7101f95b7557b61ecda
sammcgrail <sammcgrail@gmail.com>
2015-06-04 11:24:15 -0400

    local vars

--- spec/trip_spec.rb
+++ spec/trip_spec.rb
@@ -1,6 +1,8 @@
 require 'rspec'
 
 class Trip
+  # use a let
+
   ## create a constructor
   def initialize(train_name)
     @train_name = train_name
@@ -15,11 +17,13 @@ end
 
 describe Trip do
   it 'has a train number' do
+    train_name = '456'
     trip = Trip.new('456')
-    expect(trip.train_name).to eq('456')
+    expect(trip.train_name).to eq(train_name)
   end
-  it 'has a different train nubmer' do
+  it 'has a different train number' do
+    train_name = '345'
     trip = Trip.new('345')
-    expect(trip.train_name).to eq('345')
+    expect(trip.train_name).to eq(train_name)
   end
 end
